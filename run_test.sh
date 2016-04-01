014c90c8ea1349b5ba6f362873af99c08995d67f
wilsonianb <bwilson@ripple.com>
2016-04-01 09:40:07 -0700

    Fix centos6 test

--- run_test.sh
+++ run_test.sh
@@ -8,12 +8,12 @@ docker run -e RPM_RELEASE=3 -e GIT_BRANCH=release -v $PWD/test/:/opt/rippled-rpm
 # source properties
 . test/build_vars
 
-# test centos7 rpm
+# test centos6 rpm
 docker run \
 -e "RPM_FILE_NAME=$rpm_file_name" \
 -e "RIPPLED_VERSION=$rippled_version" \
 -e "MD5SUM=$md5sum" \
--v $PWD/test:/opt/rippled/in --entrypoint /opt/rippled/in/test_centos7_rpm.sh \
+-v $PWD/test:/opt/rippled/in --entrypoint /opt/rippled/in/test_centos6_rpm.sh \
 -w /opt/rippled \
 centos:6
 
